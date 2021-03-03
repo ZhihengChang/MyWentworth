f85ee73295030ec7e647f65645e4afdc5776621c
ZhihengChang <zhihengchang602@gmail.com>
2021-03-03 12:52:11 -0500

    server: changz add userRoutes to user api

--- src/server/app.js
+++ src/server/app.js
@@ -2,8 +2,15 @@
 const express = require("express");
 const config = require("../config/config.json");
 
+//Routes
+const userRoutes = require("./routes/userRoutes");
+
 const app = express();
 
+//Mounting
+app.use("/api/v1/user", userRoutes);
+
+//Start Server
 app.listen(config.service.port, ()=>{
     console.log(`Server is now running on port ${config.service.port}`);
 });
