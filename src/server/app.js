36fef98346b96205ba5742c3da58b9aa85159eb2
ZhihengChang <zhihengchang602@gmail.com>
2021-03-03 21:04:44 -0500

    server: changz add validateUserid and validateAuthToken to userController

--- src/server/app.js
+++ src/server/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 
 //Routes
 const userRoutes = require("./routes/userRoutes");
@@ -6,6 +7,8 @@ const userRoutes = require("./routes/userRoutes");
 const app = express();
 
 //Mounting
+app.use(morgan("dev"))
+app.use(express.json());
 app.use("/api/v1/user", userRoutes);
 
 module.exports = app;
--- src/server/routes/userRoutes.js
+++ src/server/routes/userRoutes.js
@@ -3,7 +3,12 @@ const userController = require("../controllers/userController");
 
 const router = express.Router();
 
+// router.use(userController.validateAuthToken);
+router.param("id", userController.validateUserId);
+
 //Routes
+router.route("/:id").get(userController.getUser);
 router.route("/login").get(userController.handleUserLogin);
+router.route("/update").post(userController.updateUser);
 
 module.exports = router;
