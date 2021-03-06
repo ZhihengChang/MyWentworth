31e4e0006ce43a66b6e89c26ed9fa6f5f034fa26
zhihengchang <zhihengchang602@gmail.com>
2021-03-06 18:35:15 -0500

    server: changz replace config.json with config.env and update server

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,29 @@
-//Set Enviorment
+//Setup Enviorment
 const dotenv = require('dotenv');
 dotenv.config({ path:  __dirname + '/../config/config.env'});
+
+//Setup logger
+const path = require('path');
+const Logger = require('../util/logger');
+const logger = new Logger(path.basename(__filename));
+// logger.details(true); //log details
+
+//DB Connection
+const mongoose = require('mongoose');
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>', 
+    process.env.DB_PASSWORD
+);
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(logger.log("Database connected!").info());
+
 const app = require("./app")
 
 const port = process.env.PORT || 3000;
 //Start Server
 app.listen(port, ()=>{
-    console.log(`Server is now running on port ${port}`);
+    logger.log(`Server is now running on port ${port}`).info();
 });
