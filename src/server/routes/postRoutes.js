24dcf0354a21da4accf94585897772d9300f6f88
zhihengchang <zhihengchang602@gmail.com>
2021-03-14 20:26:25 -0400

    server: changz add protection to create post

--- src/server/routes/postRoutes.js
+++ src/server/routes/postRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const postController = require("../controllers/postController");
+const authController = require("../controllers/authController");
 
 const router = express.Router();
 
 router.route('/')
     .get(postController.getAllPosts)
-    .post(postController.createPost);
+    .post(authController.protect, postController.createPost);
 
 module.exports = router;
