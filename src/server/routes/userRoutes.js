6f57a73e9f4aa1ae57c954483106e407e24bb706
zhihengchang <zhihengchang602@gmail.com>
2021-03-23 20:59:29 -0400

    server: changz add updatePassword route

--- src/server/routes/userRoutes.js
+++ src/server/routes/userRoutes.js
@@ -7,6 +7,10 @@ const router = express.Router();
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 
+router.patch('/updatePassword',
+    authController.protect,
+    authController.updatePassword
+);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
