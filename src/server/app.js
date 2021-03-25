8a673f20e7d87f3c0f0a2ea8a7afe8161a7eedc0
zhihengchang <zhihengchang602@gmail.com>
2021-03-24 23:17:42 -0400

    server: changz handle security issues

--- src/server/app.js
+++ src/server/app.js
@@ -1,9 +1,14 @@
-//utils
+// utils
 const AppError = require('../util/error/appError');
 const Logger = require('../util/logger');
 const globalErrorHandler = require('./controllers/errorController');
 
-//app
+// app
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require("xss-clean");
+const hpp = require("hpp");
 const expressLayouts = require("express-ejs-layouts");
 const flash = require("connect-flash");
 const express = require("express");
@@ -11,37 +16,64 @@ const morgan = require("morgan");
 const path = require("path");
 const logger = new Logger();
 
-//Routes
+// Routes
 const userRoutes = require("./routes/userRoutes");
 const postRoutes = require("./routes/postRoutes");
 
 const app = express();
 
-//Get views path
+// Global Middlewares
+app.use(helmet());
+
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
+const limiter = rateLimit({
+    max: process.env.MAX_REQ_LIMIT,
+    windowMs: process.env.LIMIT_DURATION * 60 * 1000, //ms
+    message: 'Too many requests from this IP, please try again later!'
+});
+
+app.use('/api', limiter);
+
+// Get views path
 const parentDir = path.join(__dirname, '../');
 const viewDir = path.join(parentDir, 'client/views');
 
-//Set views path
+// Set views path
 app.set('views', viewDir);
 
-//Set EJS
+// Set EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs'); 
 
-//Serve static files
+// Serve static files
 const clientDir = path.join(parentDir, 'client');
 app.use(express.static(clientDir))
 
 // Built-in bodyParser middleware
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '10Kb' }));
 
-//Mounting
-app.use(morgan("dev"))
-app.use(express.json());
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+    hpp({
+        whitelist: [] // Whitelist certain parameters
+    })
+);
+
+// Mounting
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
-//Error handling
+// Error handling 
 app.all('*', (req, res, next) => {
     next(new AppError(`Can't find ${req.originalUrl} on the server`, 404));
 });
