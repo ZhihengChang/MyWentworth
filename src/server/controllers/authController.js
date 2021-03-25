cbfd081664b5bad2aef9dd3ab053374b70a46fc2
zhihengchang <zhihengchang602@gmail.com>
2021-03-24 23:19:05 -0400

    server: changz update createTokenAndSend to send JWT via cookie

--- src/server/controllers/authController.js
+++ src/server/controllers/authController.js
@@ -35,7 +35,21 @@ const signToken = function(id){
  */
 const createTokenAndSend = function(res, statusCode, user){
     const token = signToken(user._id);
+    const cookieOptions = {
+        expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true,
+    };
 
+    if(process.env.NODE_ENV.trim() === 'production'){
+        cookieOptions.secure = true;
+    }
+
+    res.cookie('jwt', token, cookieOptions);
+    
+    user.password = undefined;
+    
     util.sendResponse(res, statusCode, {
         status: "success",
         token,
