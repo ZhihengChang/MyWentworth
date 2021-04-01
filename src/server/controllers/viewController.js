f1dd94802d967dc7a8c5e9d733e38b0f829862e0
zhihengchang <zhihengchang602@gmail.com>
2021-03-31 23:55:23 -0400

    server: changz add home page route and rendering

--- src/server/controllers/viewController.js
+++ src/server/controllers/viewController.js
@@ -5,9 +5,26 @@
  * descriptions: This controller provides dynamic page rendering
  */
 const util = require('../../util/server_utilities');
+const catchAsync = require('../../util/error/catchAsync');
+
+const Post = require('../models/postModel');
+
 
 exports.renderUserLogin = function(req, res){
     util.renderPage(res, 200, 'login', {
         title: "user Login"
     })
 }
+
+exports.renderUserHome = catchAsync( async function(req, res) {
+    // Get all public posts
+    const posts = await Post.find().sort( { post_ts: -1 });
+
+    // Build template
+
+    // Rendering
+    util.renderPage(res, 200, 'home', {
+        title: "Home",
+        posts
+    })
+});
--- src/server/routes/viewRoutes.js
+++ src/server/routes/viewRoutes.js
@@ -4,5 +4,6 @@ const viewController = require('../controllers/viewController')
 const router = express.Router();
 
 router.get('/', viewController.renderUserLogin);
+router.get('/home', viewController.renderUserHome);
 
 module.exports = router;
